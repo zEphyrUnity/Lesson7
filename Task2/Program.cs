5ec0d2fa5a9b66113555699690043db88b28534a
Igor <61108251+zEphyrUnity@users.noreply.github.com>
2020-03-09 13:47:34 +0300

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Task2
+{
+    public class MultiFormContext : ApplicationContext
+    {
+        private int openForms;
+        public MultiFormContext(params Form[] forms)
+        {
+            openForms = forms.Length;
+
+            foreach (var form in forms)
+            {
+                form.FormClosed += (s, args) =>
+                {
+                    //When we have closed the last of the "starting" forms, 
+                    //end the program.
+                    if (Interlocked.Decrement(ref openForms) == 0)
+                        ExitThread();
+                };
+
+                form.Show();
+            }
+        }
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new MultiFormContext(new Form1(), new Form2()));
+
+        }
+    }
+}
